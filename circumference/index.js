459e2c46a82e4d20d12201a94d0461c8b3d7a8dc
DORA VENKATA LAKSHMI VENNELA <vennidora2@gmail.com>
2026-03-27 16:25:25 +0530

    Delete circumference directory

--- circumference/index.js
+++
@@ -1,9 +0,0 @@
-const PI=3.14;
-let radius;
-let circumference;
-document.getElementById("submit").onclick=function() {
-    radius=document.getElementById("mytext").value;
-    radius=Number(radius);
-    circumference=2*PI*radius;
-    document.getElementById("myid").textContent=circumference+"cm";
-}
